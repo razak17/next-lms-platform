42efd907768bedb9a37b7a85780e203090e161a0
Abdul Razak <moabdulrazak17@gmail.com>
2025-07-17 17:32:26 +0000

    chore(dashboard): heading for courses page

--- app/admin/(site)/courses/page.tsx
+++ app/admin/(site)/courses/page.tsx
@@ -1,7 +1,14 @@
+import { Heading } from "@/components/ui/heading";
+
 export default async function CoursesPage() {
 	return (
-		<div>
-			<h1>Courses Page</h1>
+		<div className="@container/main flex flex-1 flex-col gap-2">
+			<div className="mt-4 flex items-center justify-between px-4 lg:px-6">
+				<Heading
+          title="Manage Courses"
+          description="Create, edit, and manage your courses effectively"
+				/>
+			</div>
 		</div>
 	);
 }
