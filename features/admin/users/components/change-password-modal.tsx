30527a31644e076c3f3b9c9dbacc54e9465bd5a9
Abdul Razak <moabdulrazak17@gmail.com>
2025-08-10 22:21:11 +0000

    fix(build): fix build errors

--- features/admin/users/components/change-password-modal.tsx
+++ features/admin/users/components/change-password-modal.tsx
@@ -26,7 +26,7 @@ import { Loader2 } from "lucide-react";
 import { useState } from "react";
 import { toast } from "sonner";
 import { z } from "zod";
-import { changePassword } from "../../../shared/actions/usersactions/users";
+import { changePassword } from "../../../shared/actions/users";
 
 interface ChangePasswordModalProps {
 	isOpen: boolean;
