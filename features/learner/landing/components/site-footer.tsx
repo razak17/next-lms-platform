6c2a3f0379077535c16b5884aed5948dcf49912b
Abdul Razak <moabdulrazak17@gmail.com>
2025-08-12 13:00:29 +0000

    feat(site): responsive footer

--- features/learner/landing/components/site-footer.tsx
+++ features/learner/landing/components/site-footer.tsx
@@ -9,48 +9,55 @@ import Link from "next/link";
 export function SiteFooter() {
 	return (
 		<footer className="bg-sidebar border-t px-4 text-center text-white">
-			<div className="flex-col px-4 lg:mx-auto lg:flex lg:w-6xl">
-				<div className="flex justify-between py-6">
-					<Icons.logoWhite className="h-32 w-72" aria-hidden="true" />
-					<div className="flex w-md">
-						<div className="flex flex-col items-start gap-4 px-4">
-							<p className="text-lg font-semibold">Menu</p>
-							{siteConfig.mainNav?.map((item) => (
+			<div className="flex flex-col px-4 lg:mx-auto lg:w-6xl">
+				<div className="flex flex-col gap-8 py-6 lg:flex-row lg:justify-between lg:gap-0">
+					<div className="flex w-full flex-col justify-between lg:flex-row">
+						<div className="flex justify-center lg:justify-start">
+							<Icons.logoWhite
+								className="h-20 w-40 sm:h-24 sm:w-48 lg:h-32 lg:w-72"
+								aria-hidden="true"
+							/>
+						</div>
+						<div className="flex flex-col gap-8 sm:flex-row sm:justify-center lg:w-lg lg:justify-end">
+							<div className="flex flex-col items-center gap-4 sm:items-start sm:px-4">
+								<p className="text-lg font-semibold">Menu</p>
+								{siteConfig.mainNav?.map((item) => (
+									<Link
+										key={item.title}
+										href={item.url}
+										className="text-secondary hover:text-white"
+									>
+										{item.title}
+									</Link>
+								))}
+							</div>
+							<div className="flex flex-col items-center gap-4 sm:items-start sm:px-4 lg:px-16">
+								<p className="text-lg font-semibold">Contact</p>
+								<p className="text-secondary">+233 123 456 789</p>
+								<p className="text-secondary text-center sm:text-left">
+									New Reiss, Ghana, Accra
+								</p>
+							</div>
+							<div className="flex flex-col items-center gap-4 sm:items-start">
+								<p className="text-lg font-semibold">Social</p>
 								<Link
-									key={item.title}
-									href={item.url}
+									href="https://linkedin.com"
 									className="text-secondary hover:text-white"
 								>
-									{item.title}
+									LinkedIn
 								</Link>
-							))}
-						</div>
-						<div className="flex flex-col items-start gap-4 px-16">
-							<p className="text-lg font-semibold">Contact</p>
-							<p className="text-secondary">+233 123 456 789</p>
-							<p className="text-secondary text-left">
-								New Reiss, Ghana, Accra
-							</p>
-						</div>
-						<div className="flex flex-col items-start gap-4">
-							<p className="text-lg font-semibold">Social</p>
-							<Link
-								href="https://linkedin.com"
-								className="text-secondary hover:text-white"
-							>
-								LinkedIn
-							</Link>
-							<Link
-								href="https://facebook.com"
-								className="text-secondary hover:text-white"
-							>
-								Facebook
-							</Link>
+								<Link
+									href="https://facebook.com"
+									className="text-secondary hover:text-white"
+								>
+									Facebook
+								</Link>
+							</div>
 						</div>
 					</div>
 				</div>
-				<div className="flex justify-between border-t py-4">
-					<p className="">
+				<div className="flex flex-col gap-4 border-t py-4 sm:flex-row sm:items-center sm:justify-between">
+					<p className="text-sm sm:text-base">
 						&copy; copyright {new Date().getFullYear()} - G-Client, All rights
 						reserved.
 					</p>
